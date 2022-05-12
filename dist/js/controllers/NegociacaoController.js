7d55a6f34eb66c0fa006f1b904938a4c8b86126e
Iuri Dias <iuridias.mw@gmail.com>
2022-05-11 21:27:34 -0300

    projeto final modulo 2

--- dist/js/controllers/NegociacaoController.js
+++ dist/js/controllers/NegociacaoController.js
@@ -26,13 +26,6 @@ export class NegociacaoController {
     verificaDiaUtil(data) {
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
     }
-    // private criaNegociacao(): Negociacao {
-    //   const regex = /-/g;
-    //   const date = new Date(this.inputData.value.replace(regex, ','));
-    //   const quantidade = parseInt(this.inputQuantidade.value);
-    //   const valor = parseFloat(this.inputValor.value);
-    //   return new Negociacao(date, quantidade, valor);
-    // }
     limparFormulario() {
         this.inputData.value = "";
         this.inputQuantidade.value = "";
